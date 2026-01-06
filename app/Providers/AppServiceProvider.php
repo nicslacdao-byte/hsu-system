09e690e662698c3d5d41456660cd95e6b121b730
nicslacdao-byte <nicslacdao@gmail.com>
2026-01-07 03:31:34 +0800

    Fix mixed content http error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- 1. ADD THIS LINE HERE
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. ADD THIS BLOCK HERE
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
